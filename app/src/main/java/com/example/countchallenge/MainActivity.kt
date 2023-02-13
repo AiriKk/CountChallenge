d8cecc23c66a72b531480a7956ca1db6d9de1de8
airik <airi.haolader@gmail.com>
2023-02-13 15:06:53 +0900

    カウントチャレンジlv.1

--- app/src/main/java/com/example/countchallenge/MainActivity.kt
+++ app/src/main/java/com/example/countchallenge/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.countchallenge
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.countchallenge.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater).apply{
+            setContentView(this.root)
+        }
+        var number: Int = 0
+        binding.numberTextview.text = number.toString()
+
+        binding.upButton.setOnClickListener{
+            number += 1
+            binding.numberTextview.text = number.toString()
+        }
     }
 }
